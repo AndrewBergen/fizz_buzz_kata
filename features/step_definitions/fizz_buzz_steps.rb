85a1449f43696f84ee39f0d37efc9efffd4dd9a5
AndrewBergen <andrewjbergen@gmail.com>
2012-11-06 15:45:17 -0600

    Fixed File Names

---
+++ features/step_definitions/fizz_buzz_steps.rb
@@ -0,0 +1,12 @@
+Given /^I have a new fizz buzz game$/ do
+  fizz_buzz_game = FizzBuzz.new
+end
+
+When /^the game starts$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^it should print the instructions$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
